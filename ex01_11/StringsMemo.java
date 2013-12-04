b153ecf0e0d228a88b0f377258291fb170916da1
ohtysk <ohtysk@gmail.com>
2013-12-04 14:24:54 +0900

    add ex01_12

--- ex01_11/StringsMemo.java
+++ ex01_11/StringsMemo.java
@@ -3,7 +3,9 @@ public class StringsMemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hoge");
+		String myName = "Yuusuke";
+		
+		myName += " Ohta";
+		System.out.println("Name = " + myName);
 	}
-
 }
---
+++ ex01_12/ImprovedFibonacci.java
@@ -0,0 +1,9 @@
+
+public class ImprovedFibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
