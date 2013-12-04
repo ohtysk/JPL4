c4cdd77bdd67b8b099bf0a4d488c86eb4a501b85
ohtysk <ohtysk@gmail.com>
2013-12-04 14:44:03 +0900

    add ex01_13

---
+++ ex01_13/ImprovedFibonacci.java
@@ -0,0 +1,25 @@
+
+public class ImprovedFibonacci {
+	static final int MAX_SIZE = 25;
+	static Number[] numbers = new Number[MAX_SIZE];
+	
+	public static void main(String[] args) {
+		int lo = 1;
+		int hi = 1;
+
+		numbers[0] = new Number(lo);
+		for (int i = 1; i < MAX_SIZE; i++) {
+			String mark = "";
+			if (hi % 2 == 0) {
+				mark = " *";
+			}
+			numbers[i] = new Number(hi + mark);
+			hi = lo + hi;
+			lo = hi - lo;
+		}
+		
+		for (int i = 0; i < MAX_SIZE; i++) {
+			System.out.printf("%d: %s%n", i + 1, numbers[i]);
+		}
+	}
+}
