9db0989286d4c5db720414e5f8b4e27baa16e4f1
ohtysk <ohtysk@gmail.com>
2013-12-11 20:30:54 +0900

    check list

--- ex02_02/LinkedList.java
+++ ex02_02/LinkedList.java
@@ -1,5 +1,22 @@
 
 public class LinkedList {
-	private Object value = null;
-	private LinkedList next = null;
+	public Object value = null;
+	public LinkedList next = null;
+	
+	public static void main(String[] args) {
+		LinkedList a = new LinkedList();
+		a.value = "hoge";
+		LinkedList b = new LinkedList();
+		b.value = "huga";
+		a.next = b;
+		System.out.println(a);
+		System.out.println(b);
+	}
+	
+	public String toString() {
+		if (next == null) {
+			return "value: " + value;
+		}
+		return  "value: " + value + " *";
+	}
 }
