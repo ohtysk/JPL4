28d3d37108a217c1d3d93a04556bcff1736187a8
ohtysk <ohtysk@gmail.com>
2013-12-11 20:12:02 +0900

    add ex02_01

---
+++ ex02_01/Vehicle.java
@@ -0,0 +1,32 @@
+
+public class Vehicle {
+	private double speed;
+	private double direction;
+	private String owner;
+	
+	public Vehicle() {}
+	
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+	
+	public double getSpeed() {
+		return speed;
+	}
+	
+	public void setDirection(double direction) {
+		this.direction = direction;
+	}
+	
+	public double getDirection() {
+		return direction;
+	}
+	
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	
+	public String getOwner() {
+		return owner;
+	}
+}
