afbbd72dea027b445df5dfe4c5046e01d476e411
ohtysk <ohtysk@gmail.com>
2013-12-15 12:55:18 +0900

    modify ex02_03

--- ex02_03/Vehicle.java
+++ ex02_03/Vehicle.java
@@ -4,8 +4,17 @@ public class Vehicle {
 	private double direction;
 	private String owner;
 	private static int nextId = 0;
-	private int id;
+	private final int id;
 	
+	public static void main(String[] args) {
+		for (int i = 10; i < 15; i++) {
+			Vehicle a = new Vehicle();
+			a.setSpeed(i * 0.5);
+			a.setDirection(i * 1.5);
+			a.setOwner("owner" + i);
+			System.out.printf("%d, %d, %f, %f, %s%n", i, a.getId(), a.getSpeed(), a.getDirection(), a.getOwner());
+		}
+	}
 	public Vehicle() {
 		id = nextId;
 		nextId++;
