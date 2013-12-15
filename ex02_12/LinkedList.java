5b5941427e83ab9e55e79056a7981504b665eb37
ohtysk <ohtysk@gmail.com>
2013-12-15 15:24:20 +0900

    modify toString() and main()

--- ex02_12/LinkedList.java
+++ ex02_12/LinkedList.java
@@ -11,19 +11,21 @@ public class LinkedList {
 	
 	public LinkedList(Object... values) {
 		int length = values.length;
-		System.out.println(length);
-		if (length > 0) {
-			System.out.println(values[0]);
-			this.value = values[0];
-			Object[] newValues = new Object[length - 1]; 
-			System.arraycopy(values, 1, newValues, 0, length -1);
+		this.value = values[0];
+		Object[] newValues = new Object[length - 1]; 
+		System.arraycopy(values, 1, newValues, 0, length -1);
+		if (length > 1) {
 			this.next = new LinkedList(newValues);
 		}
 	}
 	
 	public static void main(String[] args) {
 		LinkedList a = new LinkedList("hoge", "huga", "test");
+		LinkedList b = new LinkedList("aaaa");
+		LinkedList c = new LinkedList(new String[] {"111", "222", null, "444"});
 		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
 	}
 	
 	public String toString() {
