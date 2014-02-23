8a47c13bd2959a56aa2bfaa183c3cd4f6dad01e1
ohtysk <ohtysk@gmail.com>
2014-02-23 23:37:48 +0900

    add properties dialog

--- gui1-2/ClockWithMenu.java
+++ gui1-2/ClockWithMenu.java
@@ -5,6 +5,7 @@ public class ClockWithMenu extends Frame {
 	private MenuItem propertiesItem;
 	private MenuItem quitItem;
 	private MenuItemListener menuItemListener = new MenuItemListener();
+	private FontManager fontManager = new FontManager();
 	
 	public ClockWithMenu(String title) {
 		// TODO Auto-generated constructor stub
@@ -50,8 +51,137 @@ public class ClockWithMenu extends Frame {
 			if (item == quitItem) {
 				dispose();
 				System.exit(0);
+			} else if (item == propertiesItem) {
+				Dialog dialog = new PropertiesDialog();
+				dialog.setBounds(200, 200, 600, 200);
+				dialog.show();
+			}
+		}
+	}
+	
+	class PropertiesDialog extends Dialog {
+		Button okButton = new Button("OK");
+		Button cancellButton = new Button("Cancel");
+		
+		public PropertiesDialog() {
+			super(new Frame(), "Properties", true);
+			// TODO Auto-generated constructor stub
+			System.out.println("dialog");
+			
+			Panel buttonPanel = new Panel();
+			buttonPanel.add(okButton);
+			buttonPanel.add(cancellButton);
+			
+			add(new FontPanel(), "Center");
+			add(buttonPanel, "South");
+			
+			okButton.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					// TODO Auto-generated method stub
+					fontManager.backupIndexes();
+					dispose();
+				}
+			});
+			
+			cancellButton.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					fontManager.rollbackIndexes();
+					dispose();
+				}
+			});
+			
+			addWindowListener(new WindowAdapter() {
+				public void windowClosing(WindowEvent event) {
+					fontManager.rollbackIndexes();
+					dispose();
+				}
+			});
+			
+			pack();
+		}
+	}
+	
+	class FontPanel extends Panel {
+		public FontPanel() {
+			add(fontManager.getFamilyList());
+			add(fontManager.getStyleList());
+			add(fontManager.getSizeList());			
+		}
+	}
+	
+	class FontManager {
+		private List familyList = new List();
+		private List styleList = new List();
+		private List sizeList = new List();
+		
+		private int currentFamily = 0;
+		private int currentStyle = 0;
+		private int currentSize = 0;
+
+		public FontManager() {
+			populateFamilies();
+			populateStyles();
+			populateSizes();
+			
+			familyList.select(currentFamily);
+			styleList.select(currentStyle);
+			sizeList.select(currentSize);
+		}
+		
+		public  Font getSelectedFont() {
+			String family = familyList.getSelectedItem();
+			int style = sizeList.getSelectedIndex();
+			int size = Integer.parseInt(sizeList.getSelectedItem());
+			return new Font(family, style, size);
+		}
+					
+		private void populateFamilies() {
+			String fontNames[] = getToolkit().getFontList();
+			for (int i = 0; i < fontNames.length; i++) {
+				familyList.add(fontNames[i]);
 			}
 		}
 		
+		private void populateStyles() {
+			styleList.add("Plain");
+			styleList.add("Bold");
+			styleList.add("Italic");
+			styleList.add("BoldItalic");
+		}
+		
+		private void populateSizes() {
+			String sizes[] = {"12", "14", "16", "18", "24", "36", "48"};
+			
+			for (int i = 0; i < sizes.length; i++) {
+				sizeList.add(sizes[i]);
+			}
+		}
+		
+		public List getFamilyList() {
+			return familyList;
+		}
+		
+		public List getStyleList() {
+			return styleList;
+		}
+		
+		public List getSizeList() {
+			return sizeList;
+		}
+		
+		private void backupIndexes() {
+			currentFamily = familyList.getSelectedIndex();
+			currentStyle = styleList.getSelectedIndex();
+			currentSize = sizeList.getSelectedIndex();
+		}
+		
+		private void rollbackIndexes() {
+			familyList.select(currentFamily);
+			styleList.select(currentStyle);
+			sizeList.select(currentSize);
+		}
 	}
 }
