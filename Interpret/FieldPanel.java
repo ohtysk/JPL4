1fb4d58d392afc15f29c802019b1568b7bc3b5f2
ohtysk <ohtysk@gmail.com>
2014-04-13 20:33:21 +0900

    FieldPanel, MethodPanel変更

--- Interpret/FieldPanel.java
+++ Interpret/FieldPanel.java
@@ -1,12 +1,35 @@
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JList;
 
 
 public class FieldPanel extends JPanel {
+	private JTextField textField;
 
 	/**
 	 * Create the panel.
 	 */
 	public FieldPanel() {
+		setLayout(null);
+		
+		JLabel label = new JLabel("\u5024");
+		label.setBounds(12, 272, 50, 13);
+		add(label);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(55, 269, 220, 19);
+		add(textField);
+		
+		JButton button = new JButton("\u8A2D\u5B9A");
+		button.setBounds(286, 266, 93, 23);
+		add(button);
+		
+		JList list = new JList();
+		list.setBounds(12, 21, 367, 236);
+		add(list);
 
 	}
 
--- Interpret/MethodPanel.java
+++ Interpret/MethodPanel.java
@@ -1,13 +1,35 @@
 import javax.swing.JPanel;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 
 public class MethodPanel extends JPanel {
+	private JTextField textField;
 
 	/**
 	 * Create the panel.
 	 */
 	public MethodPanel() {
+		setLayout(null);
+		
+		JList list = new JList();
+		list.setBounds(12, 21, 367, 236);
+		add(list);
+		
+		JLabel label = new JLabel("\u5F15\u6570");
+		label.setBounds(12, 272, 50, 13);
+		add(label);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(55, 269, 220, 19);
+		add(textField);
+		
+		JButton button = new JButton("\u5B9F\u884C");
+		button.setBounds(286, 266, 93, 23);
+		add(button);
 
 	}
-
 }
