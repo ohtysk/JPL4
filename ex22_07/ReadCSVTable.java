2b0fd77f6fb77b017fada72859d7a47f156dafe5
ohtysk <ohtysk@gmail.com>
2014-08-18 12:28:54 +0900

    add ex22_07

---
+++ ex22_07/ReadCSVTable.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+
+
+public class ReadCSVTable {
+	
+	public static List<String[]> readCSVTable(Readable source, int cells) throws IOException {
+		if (cells < 1) throw new InvalidParameterException();
+		Scanner in = new Scanner(source);
+		List<String[]> vals = new ArrayList<String[]>();
+		String exp = "^(.*)";
+		for (int i = 1; i < cells; i++)
+			exp += ",(.*)";
+		Pattern pat = Pattern.compile(exp, Pattern.MULTILINE);
+		while (in.hasNextLine()) {
+			String line = in.findInLine(pat);
+			if (line != null) {
+				String[] cellsStr = new String[cells];
+				MatchResult match = in.match();
+				for (int i = 0; i < cells; i++) {
+					cellsStr[i] = match.group(i + 1);
+				}
+				vals.add(cellsStr);
+				if (in.hasNextLine()) in.nextLine();
+			} else {
+				throw new IOException("input format error");
+			}
+		}
+		
+		IOException e = in.ioException();
+		if (e != null) {
+			throw e;
+		}		return vals;
+	}
+}
