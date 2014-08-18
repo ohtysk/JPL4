47ec84cd39400060040bae7d7a615982b581f406
ohtysk <ohtysk@gmail.com>
2014-08-18 17:32:01 +0900

    add ex22_12

---
+++ ex22_12/ReadAttrsTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.Test;
+
+
+public class ReadAttrsTest {
+
+	@Test
+	public void test() throws IOException {
+		Map<String, Object> map = ReadAttrs.readAttrs(new FileReader(new File("data.attr")));
+		for(Entry<String, Object> e : map.entrySet()) {
+			System.out.println(e.getKey() + " : " + e.getValue());
+		}
+	}
+
+}
